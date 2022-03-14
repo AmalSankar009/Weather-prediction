3aef2376a4500ac47f380c12994937297e2b8ad2
AmalSankar009 <89658953+AmalSankar009@users.noreply.github.com>
2022-03-14 15:11:24 +0530

    Update weather_Train.py

--- WEATHER_PREDICTION USING [ ML ]CODE/weather_Train.py
+++ WEATHER_PREDICTION USING [ ML ]CODE/weather_Train.py
@@ -3,19 +3,26 @@ from keras.layers import Dense, Activation
 from keras.utils import np_utils
 import numpy as np
 
+# data loading 
 seed = 7
 np.random.seed(seed)
 data = np.loadtxt("data.txt")
+
+# data split to train data and test data
+# x - data Y-outcome
 X_train = data[:4646,:12]
 Y_train = data[:4646,12:13]
 X_test = data[4646:6936,:12]
 Y_test = data[4646:6936,12:13]
+
+# checking 
 len_y_train = len(Y_train)
 len_y_test = len(Y_test)
 
 print(len(Y_train))
 print(len(Y_test))
 
+# proper  data labelling 
 for i in range(0,len_y_train):
     if(Y_train[i]==1000):
         Y_train[i] = 3
@@ -47,25 +54,40 @@ for i in range(0,len_y_test):
 print(Y_train)
 print(Y_test)
 
+# converting data to numpy array of type int32
 Y_train = Y_train.astype('int32')
 Y_train = np_utils.to_categorical(Y_train,4)
 Y_test = Y_test.astype('int32')
 Y_test = np_utils.to_categorical(Y_test,4)
+
+# initialize a sequengial model
 model = Sequential()
+# adding model leyers and defining model archetecture
 model.add(Dense(100, input_dim=12, init='uniform', activation='relu'))
 model.add(Dense(80, init='uniform', activation='relu'))
 model.add(Dense(60, init='uniform', activation='relu'))
 model.add(Dense(60, init='uniform', activation='relu'))
 model.add(Dense(4))
 model.add(Activation('softmax'))
+
+# printing out model summery
 model.summary()
+
+# compile model archetecture into actual model 
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+# fit => training. fit model according to tha data
 model.fit(X_train, Y_train, nb_epoch=200, batch_size=10, verbose=2, validation_data=(X_test,Y_test))
+
+# model evaluate to check for accuracy
 scores = model.evaluate(X_test, Y_test, verbose=0)
 
 print("\n")
+
+# save trained model for future prediction
 print("%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
 
+# save model with weights 
 json_string = model.to_json()
 open('model.json', 'w').write(json_string)
 model.save_weights('data.h5',overwrite=True)
